5211875f78a2813b70be0f48459d0e90f9984219
Jin Huang <jhuang@bnl.gov>
2021-01-02 15:59:12 -0500

    local setup macro

--- setup.sh
+++ setup.sh
@@ -3,4 +3,50 @@
 which python
 python --version
 
+echo "-------------------------------------"
+echo "Install virtual env ./env"
+echo "-------------------------------------"
+
+python -m venv ./env
+source ./env/bin/activate 
+
+echo "-------------------------------------"
+echo "Install local jupyterlab"
+echo "-------------------------------------"
+
+pip install --upgrade pip
+pip install wheel
+pip install jupyterlab, ipywidgets
+
+pip freeze
+ 
+echo "-------------------------------------"
+echo "Install jupyterlab kernel for sPHENIX ROOT"
+echo "-------------------------------------"
+
+kernel_path='~/.local/share/jupyter/kernels/sphenix-root/';
+
+if [ -d $kernel_path ] 
+then
+    echo "Will use the sPHENIX ROOT kernel spec at $kernel_path" 
+else
+		echo "WARNING: this macro will install the sPHENIX ROOT jupyter kernel to your home folder:"
+		
+		rsync -avl .local/ ~/.local/
+		ls -lhvc  $kernel_path
+fi
+
+echo "-------------------------------------"
+echo "Env checks:"
+echo "-------------------------------------"
+
+which python
+python --version
+
+which ipython
+which jupyter
+
+jupyter --version
+
+jupyter kernelspec list     
 
