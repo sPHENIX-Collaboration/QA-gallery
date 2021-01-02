bce1f490b554aa2eb01b957f230edaa09385566f
Jin Huang <jhuang@bnl.gov>
2021-01-02 17:03:32 -0500

    switch to python notebook

--- QA_Draw_ALL.sh
+++ QA_Draw_ALL.sh
@@ -4,11 +4,9 @@ echo "-----------------------"
 echo "Common block: env check"
 echo "-----------------------"
 
-
 jupyter --version
 
 jupyter kernelspec list     
-
 
 echo "-----------------------"
 echo "Specific notebook updates"
