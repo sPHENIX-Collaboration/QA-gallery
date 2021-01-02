2bf3db909137980868b551aa7e3e44c51d351504
Jin Huang <jhuang@bnl.gov>
2021-01-02 16:17:46 -0500

    run calo notebook

--- QA_Draw_ALL.sh
+++ QA_Draw_ALL.sh
@@ -14,5 +14,7 @@ echo "-----------------------"
 echo "Specific notebook updates"
 echo "-----------------------"
 
+jupyter nbconvert --to notebook --inplace --execute QA-calorimeter.ipynb
+jupyter nbconvert --to html --template classic      QA-calorimeter.ipynb 
 
 
